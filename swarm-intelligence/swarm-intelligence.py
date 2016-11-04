838456f90e831509798acd12f2c4aceb699e9928
moha187@hotmail.de <moha187@hotmail.de>
2016-11-04 13:09:34 +0100

    w

--- swarm-intelligence/swarm-intelligence.py
+++ swarm-intelligence/swarm-intelligence.py
@@ -1,12 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1111@localhost:3306/'
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World! This is Swarm-Intelligence'
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
 
-if __name__ == '__main__':
-    app.run()
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+db.create_all()
+admin = User('admin','ggg')
+
+db.session.add(admin)
+db.session.commit()
