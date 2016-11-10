a3e521876b342268a1df75443f12fe000014abd8
Felix Borst <felixborst94@gmail.com>
2016-11-10 16:00:13 +0100

    finished endpoint protection using flask-httpauth

--- swarm_intelligence_app/app.py
+++ swarm_intelligence_app/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_restful import Api
 from swarm_intelligence_app.models.models import db
 from swarm_intelligence_app.resources import user
 from swarm_intelligence_app.common import errors
 from swarm_intelligence_app.common import handlers
+from swarm_intelligence_app.common.oauth import auth
+from oauth2client import client, crypt
+
 
 def load_config(app):
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost:3306/swarm_intelligence_test'
@@ -45,6 +48,30 @@ def populate():
 @app.route("/signin")
 def signin():
     return render_template('google.html')
+
+@auth.verify_token
+def verify_token(token):
+    CLIENT_ID = "92860260003-c8pdh6l63it0g2n84g7ksf7knu1ttge6.apps.googleusercontent.com"
+    try:
+        idinfo = client.verify_id_token(token, CLIENT_ID)
+        # If multiple clients access the backend server:
+        #if idinfo['aud'] not in [ANDROID_CLIENT_ID, IOS_CLIENT_ID, WEB_CLIENT_ID]:
+        #    raise crypt.AppIdentityError("Unrecognized client.")
+        #if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+        #    raise crypt.AppIdentityError("Wrong issuer.")
+        #if idinfo['hd'] != APPS_DOMAIN_NAME:
+        #    raise crypt.AppIdentityError("Wrong hosted domain.")
+    except crypt.AppIdentityError:
+    # Invalid token
+    #    userid = idinfo['sub']
+        return False
+    print(token)
+    return True
+
+@app.route("/hello")
+@auth.login_required
+def hello():
+    return "hello"
 
 if __name__ == "__main__":
     app.run('localhost', 5000, debug=True)
--- swarm_intelligence_app/resources/user.py
+++ swarm_intelligence_app/resources/user.py
@@ -2,6 +2,7 @@ from flask_restful import Resource, reqparse
 from swarm_intelligence_app.models.models import db
 from swarm_intelligence_app.models.user import User as UserModel
 from swarm_intelligence_app.common import errors
+from swarm_intelligence_app.common.oauth import auth
 
 parser = reqparse.RequestParser(bundle_errors=True)
 parser.add_argument('firstname', required=True)
@@ -10,44 +11,49 @@ parser.add_argument('email', required=True)
 
 
 class UserList(Resource):
-	def post(self):
-		args = parser.parse_args()
-		user = UserModel(
-			args['firstname'],
-			args['lastname'],
-			args['email']
-		)
-		db.session.add(user)
-		db.session.commit()
-		return {'users': 'create'}
+    @auth.login_required
+    def post(self):
+        args = parser.parse_args()
+        user = UserModel(
+            args['firstname'],
+            args['lastname'],
+            args['email']
+        )
+        db.session.add(user)
+        db.session.commit()
+        return {'users': 'create'}
 
-	def get(self):
-		users = UserModel.query.all()
-		list = [i.serialize for i in users]
-		return {'users': list}
+    @auth.login_required
+    def get(self):
+        users = UserModel.query.all()
+        list = [i.serialize for i in users]
+        return {'users': list}
 
 class User(Resource):
-	def get(self, user_id):
-		user = UserModel.query.get(user_id)
-		if user == None:
-			raise errors.EntityNotFoundError('User', user_id)
-		return {'users': [user.serialize]}
+    @auth.login_required
+    def get(self, user_id):
+        user = UserModel.query.get(user_id)
+        if user == None:
+            raise errors.EntityNotFoundError('User', user_id)
+        return {'users': [user.serialize]}
 
-	def put(self, user_id):
-		args = parser.parse_args()
-		user = UserModel.query.get(user_id)
-		if user == None:
-			raise errors.EntityNotFoundError('User', user_id)
-		user.firstname = args['firstname']
-		user.lastname = args['lastname']
-		user.email = args['email']
-		db.session.commit()
-		return {'users': 'update'}
+    @auth.login_required
+    def put(self, user_id):
+        args = parser.parse_args()
+        user = UserModel.query.get(user_id)
+        if user == None:
+            raise errors.EntityNotFoundError('User', user_id)
+        user.firstname = args['firstname']
+        user.lastname = args['lastname']
+        user.email = args['email']
+        db.session.commit()
+        return {'users': 'update'}
 
-	def delete(self, user_id):
-		user = UserModel.query.get(user_id)
-		if user == None:
-			raise errors.EntityNotFoundError('User', user_id)
-		db.session.delete(user)
-		db.session.commit()
-		return {'users': 'delete'}
+    @auth.login_required
+    def delete(self, user_id):
+        user = UserModel.query.get(user_id)
+        if user == None:
+            raise errors.EntityNotFoundError('User', user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return {'users': 'delete'}
